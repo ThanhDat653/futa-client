03a3e5ed28fcccff97a94f0987c9837b16e214d5
NTKH <hn26677@gmail.com>
2024-10-21 13:26:02 +0700

    upd

--- app/lien-he/page.tsx
+++ app/lien-he/page.tsx
@@ -1,27 +1,17 @@
 import React from 'react'
 
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from '@/components/ui/collapsible'
-
 function Page() {
     return (
-        <div className="flex flex-col gap-12 bg-white pb-12 pt-8 text-base text-black sm:flex-row">
+        <div className="container mx-auto flex flex-col gap-12 bg-white pb-12 pt-8 text-base text-black sm:flex-row">
             <div className="flex flex-col sm:min-h-[450px] sm:min-w-[300px] sm:max-w-[413px]">
                 <div className="pl-4 text-lg font-bold uppercase sm:pl-0">
                     Liên hệ chúng tôi
                 </div>
-                <Collapsible>
-                    <CollapsibleTrigger>
-                        Can I use this in my project?
-                    </CollapsibleTrigger>
-                    <CollapsibleContent>
-                        Yes. Free to use for personal and commercial projects.
-                        No attribution required.
-                    </CollapsibleContent>
-                </Collapsible>
+                <div className="flex flex-col gap-2 pl-2 text-base leading-6">
+                    <div className="text-blue-500">
+                        CÔNG TY CỔ PHẦN XE KHÁCH PHƯƠNG TRANG - FUTA BUS LINES
+                    </div>
+                </div>
             </div>
             <div className=""></div>
         </div>
